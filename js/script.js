fd4615bf05d0543a1cb5cf3ab28e94a04b37a458
Ahmet COLAK <acolak@netas.com.tr>
2020-11-08 00:40:19 +0300

    export png function

--- js/script.js
+++ js/script.js
@@ -6,3 +6,16 @@ document.querySelectorAll('div').forEach(element => {
         element.style.color = 'black'
     }
 });
+
+
+function exportAsPNG() {
+    console.log();
+    html2canvas(document.querySelector("body")).then(canvas => {
+        canvas.toBlob(function (blob) {
+            saveAs(blob, "git-cheat-sheet.png");
+            let file = new Blob([blob], { type: "application/octet-stream" });
+            let blobURL = URL.createObjectURL(file);
+            window.location.href = blobURL;
+        })
+    });
+}
