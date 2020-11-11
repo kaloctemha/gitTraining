0515d2fe437bbc0fc5dae2e121c538a23568bde9
Ahmet COLAK <acolak@netas.com.tr>
2020-11-11 21:47:44 +0300

    replace mousehover script with css hover

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-document.querySelectorAll('div').forEach(element => {
-    element.onmouseover = () => {
-        element.style.color = 'red'
-    }
-    element.onmouseout = () => {
-        element.style.color = 'black'
-    }
-});
+// document.querySelectorAll('div').forEach(element => {
+//     element.onmouseover = () => {
+//         element.style.color = 'red'
+//     }
+//     element.onmouseout = () => {
+//         element.style.color = 'black'
+//     }
+// });
 
 
 function exportAsPNG() {
